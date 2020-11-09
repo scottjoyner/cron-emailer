89ace7137a1f31cb2f3c2c2871445f21d751ed57
Scott Joyner <kipnerter@gmail.com>
2020-11-09 12:49:31 -0500

    Base Emailer systemusing cron jobs

---
+++ README.md
@@ -0,0 +1,23 @@
+## CRONJOB Emailer Service
+![https://ethereal.email/](https://ethereal.email/)
+
+## How to run
+1. Clone the repository and navigate to the base directory.
+```
+git clone https://github.com/scottjoyner/cron-emailer.git
+cd cron-emailer
+```
+2. Install the node packages needed to run the application
+```
+yarn install
+```
+3. Rename the .envExample file to .env and add in the information needed to runthe application.
+
+4. Specify the interval of the emails arival and other settings
+
+5. Run the application
+```
+node index.js
+```
+
+
---
+++ index.js
@@ -0,0 +1,50 @@
+const cron = require("node-cron")
+const express = require("express")
+const nodemailer = require("nodemailer")
+const dotenv = require('dotenv')
+dotenv.config()
+const {SENDER_EMAIL} = process.env
+const {SENDER_PASS} = process.env
+const {RECEIVER_LIST} = process.env
+const {CC_LIST} = process.env
+const {SUBJECT} = process.env
+const {HTML} = process.env
+
+// console.log(SENDER_EMAIL, SENDER_PASS, RECEIVER_LIST, CC_LIST, SUBJECT, HTML)
+app = express()
+
+let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: SENDER_EMAIL,
+        pass: SENDER_PASS
+    }
+})
+
+let mailOptions = {
+    from: SENDER_EMAIL,
+    to: RECEIVER_LIST,
+    cc: "blkscj@gmail.com",
+    subject: SUBJECT,
+    html: HTML
+}
+
+cron.schedule("* * * * *", () => {
+    console.log("Running Cron Job " + new Date())
+    sendEmail()
+})
+
+sendEmail = () => {
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            throw error
+        } else {
+            console.log(`
+            Email successfully sent at ${new Date()}
+            Info: ${info.response}
+            `)
+        }
+    })
+}
+
+app.listen("3128")
